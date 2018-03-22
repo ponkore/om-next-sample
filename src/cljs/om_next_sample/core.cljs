cfb6ca12550aeee94b0e3dddeb030c98ed3bef66
ponkore <ponkore@gmail.com>
2018-03-22 20:30:43 +0900

    convert to sablono/html.

--- src/cljs/om_next_sample/core.cljs
+++ src/cljs/om_next_sample/core.cljs
@@ -1,6 +1,7 @@
 (ns om-next-sample.core
   (:require [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]))
+            [om.dom :as dom]
+            [sablono.core :refer [html]]))
 
 (enable-console-print!)
 
@@ -9,7 +10,10 @@
 (defui RootComponent
   Object
   (render [this]
-    (dom/div nil (dom/h1 nil (:text @app-state)))))
+    (html
+     [:div
+      [:h1
+       (:text @app-state)]])))
 
 (def root (om/factory RootComponent))
 
