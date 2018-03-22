3137cb2dd44ba5a794ab6ad7396cdb9ac55e2411
ponkore <ponkore@gmail.com>
2018-03-23 00:31:55 +0900

    add sample codes.

--- src/cljs/om_next_sample/core.cljs
+++ src/cljs/om_next_sample/core.cljs
@@ -5,17 +5,52 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(defonce app-state (atom {:root/text "Hello Chestnut!"
+                          :root/foo-data {:foo/my-id "sample sample"}}))
 
-(defui RootComponent
+(defmulti read om/dispatch)
+(defmulti mutate om/dispatch)
+
+(defmethod read :default
+  [{:keys [state] :as env} k params]
+  {:value (let [_ (.log js/console "read key=" (name k) " => " (get @state k))]
+            (get @state k))})
+
+(def parser (om/parser {:read read :mutate mutate}))
+
+(def reconciler
+  (om/reconciler
+   {:state app-state
+    :normalize true
+    ;; :merge-tree (fn [a b] (debug "|merge" a b) (merge a b))
+    :parser parser}))
+
+(defui ^:once Foo
+  static om/IQuery
+  (query [this]
+    [:foo/my-id])
   Object
   (render [this]
-    (html
-     [:div
-      [:h1
-       (:text @app-state)]])))
+    (let [{:keys [foo/my-id]} (om/props this)]
+      (html
+       [:div
+        [:h2 (str my-id)]]))))
 
-(def root (om/factory RootComponent))
+(def foo (om/factory Foo))
+
+(defui ^:once RootComponent
+  static om/IQuery
+  (query [this]
+    `[:root/text
+      {:root/foo-data ~(om/get-query Foo)}])
+  Object
+  (render [this]
+    (let [{:keys [root/text root/foo-data]} (om/props this)]
+      (html
+       [:div
+        [:h1
+         text]
+       (foo foo-data)]))))
 
 (defn render []
-  (js/ReactDOM.render (root) (js/document.getElementById "app")))
+  (om/add-root! reconciler RootComponent (js/document.getElementById "app")))
