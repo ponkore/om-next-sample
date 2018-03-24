3ea6460017097d4f46d46003365acb37ec28d0da
ponkore <ponkore@gmail.com>
2018-03-24 14:38:51 +0900

    text component fix

--- src/cljs/om_next_sample/text.cljs
+++ src/cljs/om_next_sample/text.cljs
@@ -3,39 +3,43 @@
             [om.dom :as dom]
             [cljsjs.material-ui]
             [cljs-react-material-ui.core :as ui]
-            [cljs-react-material-ui.icons :as ic]))
+            [cljs-react-material-ui.icons :as ic]
+            [taoensso.timbre :refer-macros [log trace debug info warn error fatal report]]))
+
+;; TODO spec による props の型付け
+;;      それと didmount 時にチェックを入れる
+;; 必須項目 :id
+;; optional :default-value :on-change-fn [this new-value]
+(def own-props [:on-change-fn])
 
 (defui TextInput
   Object
-  (componentDidMount [this]
-    (let [{:keys [textinput/text]} (om/props this)]
+  (componentWillMount [this]
+    (let [{:keys [text]} (om/props this)]
       (om/update-state! this assoc
                         :composing? (atom false)
-                        :old-val (atom text))
-      (when-not (empty? text)
-        (let [this-node (-> (om/react-ref this "this-node-ref") dom/node)]
-          (set! (.-value this-node) text)))))
+                        :old-val (atom text))))
   (componentWillUnmount [this]
     (let [{:keys [composing? old-val]} (om/get-state this)]
       (reset! old-val "")
       (reset! composing? false)))
   (render [this]
     (let [{:keys [on-change-fn]} (om/props this)
-          on-change-fn (if on-change-fn on-change-fn (fn [_ _] ))
-          {:keys [composing? old-val]} (om/get-state this)]
-      (ui/text-field
-       {:id "xxxxx"
-        :ref "this-node-ref"
-        :on-composition-start (fn [event] (reset! composing? true))
-        :on-composition-end (fn [event]
-                              (let [v (-> event .-target .-value)]
-                                (reset! composing? false)
-                                (when-not (= v @old-val)
-                                  (reset! old-val v)
-                                  (on-change-fn this v))))
-        :on-input (fn [event]
-                    (let [v (-> event .-target .-value)]
-                      (when-not @composing?
-                        (when-not (= v @old-val)
-                          (reset! old-val v)
-                          (on-change-fn this v)))))}))))
+          on-change-fn (or on-change-fn (fn [_ _] ))
+          {:keys [composing? old-val]} (om/get-state this)
+          props (apply dissoc (om/props this) own-props)
+          props (assoc props
+                       :on-composition-start (fn [event] (reset! composing? true))
+                       :on-composition-end (fn [event]
+                                             (let [v (-> event .-target .-value)]
+                                               (reset! composing? false)
+                                               (when-not (= v @old-val)
+                                                 (reset! old-val v)
+                                                 (on-change-fn this v))))
+                       :on-input (fn [event]
+                                   (let [v (-> event .-target .-value)]
+                                     (when-not @composing?
+                                       (when-not (= v @old-val)
+                                         (reset! old-val v)
+                                         (on-change-fn this v))))))]
+      (ui/text-field props))))
