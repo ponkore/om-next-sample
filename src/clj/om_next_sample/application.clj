d7943c4c81aa3ae3385073b56925acd20f9c7ad9
ponkore <ponkore@gmail.com>
2018-03-25 19:57:53 +0900

    routing library switched from compojure to bidi.

--- src/clj/om_next_sample/application.clj
+++ src/clj/om_next_sample/application.clj
@@ -12,7 +12,7 @@
   (component/system-map
    :routes     (new-endpoint home-routes)
    :middleware (new-middleware {:middleware (:middleware config)})
-   :handler    (-> (new-handler)
+   :handler    (-> (new-handler :router :bidi)
                    (component/using [:routes :middleware]))
    :http       (-> (new-web-server (:http-port config))
                    (component/using [:handler]))))
--- src/clj/om_next_sample/routes.clj
+++ src/clj/om_next_sample/routes.clj
@@ -1,15 +1,42 @@
 (ns om-next-sample.routes
   (:require [clojure.java.io :as io]
-            [compojure.core :refer [ANY GET PUT POST DELETE routes]]
-            [compojure.route :refer [resources]]
-            [ring.util.response :refer [response]]))
+            [bidi.bidi :as bidi]
+            [bidi.ring :refer [resources]]
+            [ring.util.response :refer [response file-response resource-response redirect]]
+            [om.next.server :as om]
+            [taoensso.timbre :as timbre :refer [log trace debug info warn error fatal]]))
 
-(defn home-routes [endpoint]
-  (routes
-   (GET "/" _
-     (-> "public/index.html"
-         io/resource
-         io/input-stream
-         response
-         (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
-   (resources "/")))
+(defmulti mutatef om/dispatch)
+(defmulti readf om/dispatch)
+
+(defmethod readf :default
+  [{:keys [state query] :as env} k params]
+  (info "state=" state ",query=" query ",env=" env ",k=" k ",params=" params)
+  {:value {:text "abc"}})
+
+(def parser (om/parser {:read readf :mutate mutatef}))
+
+(defn generate-response [data & [status]]
+  {:status  (or status 200)
+   :headers {"Content-Type" "application/transit+json; charset=UTF-8"}
+   :body    data})
+
+(defn api
+  [req]
+  (let [{:keys [state]} req]
+    (generate-response
+      (parser {:state state} (:remote (:transit-params req))))))
+
+(defn index-handler
+  [req]
+  (assoc (resource-response "index.html" {:root "public"})
+         :headers {"Content-Type" "text/html; charset=UTF-8"}))
+
+(def routes ["/" {"" {:get index-handler}
+                  "css" {:get (resources {:prefix "public/css/"})}
+                  "js" {:get (resources {:prefix "public/js/"})}
+                  "api" {:get  {[""] api}}}])
+
+(defn home-routes
+  [endpoint]
+  routes)
